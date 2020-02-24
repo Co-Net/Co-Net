937b294e8b085fb05b2e617b6127c8339e116412
Juju Liao <julian.liao@sjsu.edu>
2020-02-23 20:41:20 -0800

    Updated frontend

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,28 +1,15 @@
 
 import Modal from '@material-ui/core/Modal';
-import React, { Component, useState } from 'react';
-import Avatar from '@material-ui/core/Avatar';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-//import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-//import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/styles';
-import { Route, BrowserRouter as Router } from 'react-router-dom'
-import Feed from "./Feed";
-import { Dimensions } from 'react';
 import logo from './logo.png';
 import bgd from './background.jpeg';
 import axios from 'axios';
+import styles from './App.css';
 
 const useStyles = makeStyles(theme => ({
   root: {
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './Login';
 import * as registerServiceWorker from './serviceWorker';
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/Login" component={Login} />
+      </div>
+    </Router>
+  );
+
+ReactDOM.render(routing, document.getElementById('root'));
 registerServiceWorker.unregister();
