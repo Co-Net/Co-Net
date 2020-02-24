5417e11608c420a3d376fdc9a0e0e38ca1475677
Juju Liao <julian.liao@sjsu.edu>
2020-02-23 16:46:20 -0800

    Fixed frontend exports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -163,3 +163,5 @@ class App extends Component {
     );
   }
 }
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as registerServiceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+registerServiceWorker.unregister();
