4de9a3c6f8a96f29668d54e3ad6f54425292a602
Juju Liao <julian.liao@sjsu.edu>
2020-02-23 16:10:31 -0800

    Init frontend

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+
+// client/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
