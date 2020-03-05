ab4526816246913130fa76fdfbed4ba2dc0abb90
Anthony Jhong <anthonyjhong@yahoo.com>
2020-03-04 22:02:25 -0800

    Completed forumPostModel

--- backend/api/models/forumPostModel.js
+++ backend/api/models/forumPostModel.js
@@ -1,8 +1,30 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ReplyIDSchema = new Schema({id: String});
+
 const ForumPostModel = new Schema({
-    
+    parentID:{
+        type: String,
+        default: 0
+    },
+    allReplyIDs: [ReplyIDSchema],
+    timePosted:{
+        dateTime: Date,
+        default: Date.now
+    },
+    body: {
+        text: String,
+        required: [true, "Please eneter the body of the post or reply!"]
+    },
+    title: {
+        name: String // only allow/require titles if the post is the first parent 
+    },
+    game: {
+        gameName: String,
+        required: [true, "Please eneter the game of this thread!"]
+    }
+
 })
 
 module.exports = mongoose.model('ForumPost', ForumPostModel);
