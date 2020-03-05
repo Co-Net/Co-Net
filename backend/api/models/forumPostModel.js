aebf7c2ea86f723aae212ccf6896e7a0111a4342
Anthony Jhong <anthonyjhong@yahoo.com>
2020-03-04 21:19:19 -0800

    Forum Post Model created

---
+++ backend/api/models/forumPostModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ForumPostModel = new Schema({
+    
+})
+
+module.exports = mongoose.model('ForumPost', ForumPostModel);
