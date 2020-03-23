97bf95d17cd9cffc0944091b018dea9b5fed79e7
Kenneth <lee.kenneth12@gmail.com>
2020-03-22 20:35:05 -0700

    hi
    hi

--- frontend/src/forumPost.js
+++ frontend/src/forumPost.js
@@ -8,10 +8,15 @@ import styles from './main.module.css';
 import profilePic from './profilePic.png';
 import Typography from '@material-ui/core/Typography';
 import Menu from './ProfileMenu.js';
+import ForumComment from './forumComment.js';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
 import CardContent from '@material-ui/core/CardContent';
+import TextField from '@material-ui/core/TextField';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 
 
 
@@ -21,13 +26,26 @@ class forumPost extends Component
   constructor(props) {
     super(props);
     this.pushHistory = this.pushHistory.bind(this);
-
+    this.state = {
+      clicks:0,
+      show:true
+    };
   }
 
   pushHistory(){
     this.props.history.push('/editprofile')
   
    }
+
+   IncrementItem = () => {
+    this.setState({ clicks: this.state.clicks + 1 });
+  }
+  DecreaseItem = () => {
+    this.setState({ clicks: this.state.clicks - 1 });
+  }
+  ToggleClick = () => {
+    this.setState({ show: !this.state.show });
+  }
    
  
   render()
@@ -40,6 +58,7 @@ class forumPost extends Component
       }
     });
 
+    
     const paperStyle = {
       marginTop: 8,
       display: 'flex',
@@ -89,7 +108,9 @@ class forumPost extends Component
       marginLeft: 8,
 
     }
+    const bull = <span className={styles.bullet}>•</span>;
     const postText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget rhoncus nunc, eget tempor purus. Praesent sollicitudin, ligula id porttitor faucibus, tortor mauris hendrerit augue, ut molestie ipsum arcu  Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n\nNulla eget rhoncus nunc, eget tempor purus. Praesent sollicitudin, ligula id porttitor faucibus, tortor mauris hendrerit augue, ut molestie ipsum arcuLorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget rhoncus nunc, eget tempor purus. Praesent sollicitudin, ligula id porttitor faucibus, tortor mauris hendrerit augue, ut molestie ipsum arcu  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget rhoncus nunc, eget tempor purus. Praesent sollicitudin, ligula id porttitor faucibus, tortor mauris hendrerit augue, ut molestie ipsum arcu'
+    
 
     return (
  <div>
@@ -100,16 +121,34 @@ class forumPost extends Component
       
 
       <Card className = {styles.postSpacing}>
-      <CardContent>
+      <CardContent className = {styles.mainPostSpacing}>
       <Grid container spacing = {1}>
       <Grid item xs ={2}>
-      <Avatar src= {profilePic} className = {styles.mediumSize} />
+      <Avatar src= {profilePic} className = {styles.avatarForum} />
+      <div className = {styles.postStats}>
+      <div>
+      <Typography className = {styles.statsText} display = "inline" >Views: {' '}</Typography>
+      <Typography  className = {styles.statsNumber} display = "inline" >56,503</Typography>
+      </div>
+  
+      <div>
+      <Typography className = {styles.statsText} display = "inline" >Replies: {' '}</Typography>
+      <Typography  className = {styles.statsNumber} display = "inline" >26</Typography>
+      </div>
+      </div>
+      <div className = {styles.arrows}>
+      <ExpandLessIcon className = {styles.upDownVote} fontsize = "large"></ExpandLessIcon>
+      <Typography className = {styles.voteNumber}>57</Typography>
+      <ExpandMoreIcon className = {styles.upDownVote} fontsize = "large"></ExpandMoreIcon>
 
+
+      </div>
+      
         </Grid>
         <Grid item xs ={10}>
 
         <Typography variant = 'h6' className = {styles.forumPostTitle} >Looking for Duo in Ranked, I'm Gold</Typography>
-        <Typography className = {styles.userName} display = "inline" >Destroyer392, </Typography>
+        <Typography className = {styles.userNameForum} display = "inline" >Destroyer392, </Typography>
         <Typography  className = {styles.timeStamp} display = "inline" >February 2, 2020</Typography>
         <hr  style={{
             color: '#ababab',
@@ -132,6 +171,34 @@ class forumPost extends Component
             marginTop: 20,
             marginBottom: 20,
         }}/>
+
+        <ForumComment></ForumComment>
+        <ForumComment></ForumComment>
+        <ForumComment></ForumComment>
+        <ForumComment></ForumComment>
+        <ForumComment></ForumComment>
+        <Grid container spacing = {8}>
+        <Grid item xs ={1}>
+        <Avatar src= {profilePic} className = {styles.smallSize} />
+
+        </Grid>
+        <Grid item xs = {10}>
+        <TextField
+        id="standard-textarea"
+        label="Leave a Comment"
+        className = {styles.commentBox}
+        multiline
+      />
+      <Button className = {styles.postComment} variant = "contained" color = "primary">Post Comment</Button>
+      
+        </Grid>
+        </Grid>
+     
+  
+        
+      
+
+
             </Grid>
 
         </Grid>
@@ -144,15 +211,51 @@ class forumPost extends Component
       <Grid item xs = {4}>
        <Card className = {styles.postSpacing}>
       <CardContent>
-      <Typography>hello</Typography>
+      <Typography variant = 'h6' className = {styles.forumPostTitle} >Related Posts</Typography>
+      <hr  style={{
+        color: '#ababab',
+        backgroundColor: '#ababab',
+        height: .5,
+        marginTop: 15,
+        marginBottom: 15,
+    }}/>
+    <div className = {styles.relatedPosts}>
+    <Typography className = {styles.relatedLinks}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget rhoncus nunc, eget tempor purus.</Typography>
+    <div className = {styles.marginTop}>
+    <Typography className = {styles.userNameForum} display = "inline" >StrongLittleOne{bull}</Typography>
+    <Typography  className = {styles.timeStamp} display = "inline" >26 Comments</Typography>
+    
+    </div>
+        
+    
+    </div>
+
+    <div className = {styles.relatedPosts}>
+    <Typography className = {styles.relatedLinks}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget rhoncus nunc, eget tempor purus.</Typography>
+    <div className = {styles.marginTop}>
+    <Typography className = {styles.userNameForum} display = "inline" >StrongLittleOne{bull}</Typography>
+    <Typography  className = {styles.timeStamp} display = "inline" >26 Comments</Typography>
+    
+    </div>
+        
+    
+    </div>
+    <div className = {styles.relatedPosts}>
+    <Typography className = {styles.relatedLinks}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget rhoncus nunc, eget tempor purus.</Typography>
+    <div className = {styles.marginTop}>
+    <Typography className = {styles.userNameForum} display = "inline" >StrongLittleOne{bull}</Typography>
+    <Typography  className = {styles.timeStamp} display = "inline" >26 Comments</Typography>
+    
+    </div>
+        
+    
+    </div>
+    
+
       </CardContent>
       </Card>
 
-       <Card className = {styles.postSpacing}>
-      <CardContent>
-      <Typography>hello</Typography>
-      </CardContent>
-      </Card>
+      
 
       </Grid>
 
