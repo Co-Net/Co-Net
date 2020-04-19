5c765d7e338f999c58859424830e5ba6504d66b8
Juju Liao <julian.liao@sjsu.edu>
2020-04-19 01:30:18 -0700

    Removed unneeded import

--- backend/api/models/gameModel.js
+++ backend/api/models/gameModel.js
@@ -4,7 +4,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const GameTagSchema = new Schema({name: String});
 const GameCommentSchema = new Schema({
     username: {
         type: String, 
