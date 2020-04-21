a578e6839a37ec5f21f1bcda6e7cc60b3abe9e7a
Anthony Jhong <anthonyjhong@gmail.com>
2020-04-21 00:56:47 -0700

    Bug Fix

--- backend/api/routes/messageThreadRouter.js
+++ backend/api/routes/messageThreadRouter.js
@@ -21,7 +21,7 @@ router.get('/:username', function (req, res) {
     });
 })
 
-router.get('/:id', function (req, res) {
+router.get('/byID/:id', function (req, res) {
     var queryID = req.params.id;
     MessageThreadModel.findById(queryID, function (err, doc) {
         if (err) return res.json({
