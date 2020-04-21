a6585d5bace3de36eee5eeee4dedaed1f1c40638
Anthony Jhong <anthonyjhong@gmail.com>
2020-04-21 02:47:10 -0700

    Mass Delete

--- frontend/src/friendsList.js
+++ frontend/src/friendsList.js
@@ -31,43 +31,42 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Review from './Userreview';
 import Friend from './friend';
 
-export default function friendList(props){
-    const titleStyle = {
-        marginBottom: 30,
-        color: "#535353",
-      }
-    
-      const gameTitle = {
-        color: "black",
-        marginTop: 20,
-        marginLeft: 8,
-    
-      }
-      const gameDesc = {
-        color: "#535353",
-        marginTop: 5,
-        marginLeft: 8,
-    
-      }
-    return(
-
-        <div className = "content">
-<Grid container spacing={3}>
-
-<Typography style = {titleStyle} align = 'Left' variant="h6" component="h2" >
-{props.title}</Typography>
-</Grid>
-<Friend></Friend>
-<Friend></Friend>
-<Friend></Friend>
-<Friend></Friend>
-<Friend></Friend>
+export default function friendList(props) {
 
 
 
+  const titleStyle = {
+    marginBottom: 30,
+    color: "#535353",
+  }
 
-</div>
-    );
+  const gameTitle = {
+    color: "black",
+    marginTop: 20,
+    marginLeft: 8,
+
+  }
+  const gameDesc = {
+    color: "#535353",
+    marginTop: 5,
+    marginLeft: 8,
+
+  }
+  return (
+
+    <div className="content">
+      <Grid container spacing={3}>
+
+        <Typography style={titleStyle} align='Left' variant="h6" component="h2" >
+          {props.title}</Typography>
+      </Grid>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
+      <Friend></Friend>
+
+    </div>
+  );
 
 }
-
