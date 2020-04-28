71506c5c1cd3ee5e6a2f7a09663477a8663c96cc
Juju Liao <liaojulian@gmail.com>
2020-04-27 19:53:32 -0700

    Removed unneeded title router

--- backend/api/models/titleModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
-
-const TitleSchema = new Schema({
-    title: String
-});
-
-module.exports = mongoose.model('Title', TitleSchema);
