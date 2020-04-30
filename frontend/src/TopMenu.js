c326cfee9dde1edc4d7544b6f9653593b2564155
Juju Liao <liaojulian@gmail.com>
2020-04-30 14:53:35 -0700

    cleanup topmenu

--- frontend/src/TopMenu.js
+++ frontend/src/TopMenu.js
@@ -23,7 +23,12 @@ import PartyButton from "./Party";
 import PartyActive from "./PartyActive";
 import socketIOClient from "socket.io-client";
 import algoliasearch from "algoliasearch/lite";
-import { InstantSearch, Hits, connectSearchBox, connectStateResults } from "react-instantsearch-dom";
+import {
+  InstantSearch,
+  Hits,
+  connectSearchBox,
+  connectStateResults,
+} from "react-instantsearch-dom";
 
 const searchClient = algoliasearch(
   "T7MES4D4M7",
@@ -102,12 +107,12 @@ const SearchBox = ({ currentRefinement, refine, onSubmit }) => {
       }}
       inputProps={{ "aria-label": "search" }}
       value={currentRefinement}
-      onChange={event => {
+      onChange={(event) => {
         // console.log(currentRefinement);
         refine(event.currentTarget.value);
       }}
       onKeyPress={(event) => {
-        if (event.key === 'Enter') {
+        if (event.key === "Enter") {
           onSubmit(currentRefinement);
           event.preventDefault();
         }
@@ -134,24 +139,6 @@ export default function PrimarySearchAppBar(props) {
   const handleSubmit = (submission) => {
     console.log(`Submit ${submission}`);
   };
-
-  // // Runs on refresh and every component load/change
-  // // Use this to check if still Active
-  // const getUser = () => {
-  //   axios
-  //     .get("http://localhost:3001/user/currentuser", { withCredentials: true })
-  //     .then((json) => {
-  //       // If already logged in, don't run
-  //       if (isGuest) {
-  //         setUsername(json.data.username);
-  //         setIsGuest(false);
-  //         const socket = socketIOClient("http://localhost:3001");
-  //         socket.on("connected", () => {
-  //           socket.emit("login", json.data.username);
-  //         });
-  //       }
-  //     });
-  // };
 
   const { history } = props;
 
@@ -236,7 +223,7 @@ export default function PrimarySearchAppBar(props) {
   );
 
   function Hit(props) {
-    console.log('hit');
+    console.log("hit");
     console.log(props.hit.username);
     return <div>{props.hit.username}</div>;
   }
@@ -465,154 +452,3 @@ export default function PrimarySearchAppBar(props) {
     </div>
   );
 }
-/*
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`nav-tabpanel-${index}`}
-      aria-labelledby={`nav-tab-${index}`}
-      {...other}
-    >
-      <Box p={3}>{children}</Box>
-    </Typography>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `nav-tab-${index}`,
-    'aria-controls': `nav-tabpanel-${index}`,
-  };
-}
-
-function LinkTab(props) {
-  return (
-    <Tab
-      component="a"
-      onClick={event => {
-        //event.SignIn();
-      }}
-      {...props}
-    />
-  );
-}
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: 'white',
-    textColor: 'black',
-    maxwidth: 500,
-  },
-
-}));
-const useStyles1 = makeStyles({
-  list: {
-    width: 250,
-  },
-  fullList: {
-    width: 'auto',
-  },
-});
-
- 
-
-export default function NavTabs() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-  const classes1 = useStyles1();
-  const [state, setState] = React.useState({
-    top: false,
-    left: false,
-    bottom: false,
-    right: false,
-  });
-
-  const toggleDrawer = (side, open) => event => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
-
-    setState({ ...state, [side]: open });
-  };
-
-  const sideList = side => (
-    <div
-      className={classes1.list}
-      role="presentation"
-      onClick={toggleDrawer(side, false)}
-      onKeyDown={toggleDrawer(side, false)}
-    >
-      <List>
-        {['Sign Up', 'Login', 'My Profile', 'Trending'].map((text, index) => (
-          <MenuItem button key={text} href="/signModal">
-            <ListItemText primary={text} href="/signModal"/>
-          </MenuItem>
-        ))}
-      </List>
-      <Divider />
-     
-    </div>
-  );
-
-  return (
-    <div className={classes.root}>
-    <div style = {{display: 'inline-flex',}}>
-    <img src = {logo} alt = "Logo" style = {{width: '16%', height: 76, marginTop: 5,}}/>
-
-    <Search />
-    
-    </div>
-    {/*<div style = {{display: 'inline-block', float: 'right', marginTop: 27, marginRight: 10,}}>
-    <Button onClick={toggleDrawer('right', true)}><MenuIcon></MenuIcon></Button>
-    <Drawer anchor="right" open={state.right} onClose={toggleDrawer('right', false)}>
-    {sideList('right')}
-  </Drawer>
-  </div>
-  }
-      <AppBar position="static" elevation={0}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          tabItemContainerStyle={{width: '400px'}}
-          aria-label="nav tabs example"
-          
-          TabIndicatorProps={{
-            style: {
-              backgroundColor: "white "
-
-            }
-          }}
-          style = {{backgroundColor: 'white', color: 'black'}}
-        >
-        
-          <LinkTab label="My Profile" href="/" {...a11yProps(3)} />
-          <LinkTab label="Trending" href="/Feed" {...a11yProps(4)} />
-          <LinkTab label="Create Post" href="/CreatePost" {...a11yProps(5)} />
-          <LinkTab label="My Feed" href="/MyFeed" {...a11yProps(6)} />
-
-        </Tabs>
-      </AppBar>
-      
-    </div>
-  );
-}
-*/
-// ReactDOM.render(routing, document.getElementById('root'))
