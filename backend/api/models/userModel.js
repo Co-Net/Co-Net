ba3b96f14939ca9c880e5a289b494e0459cce74f
Juju Liao <liaojulian@gmail.com>
2020-05-03 01:15:53 -0700

    fixed gametags schema

--- backend/api/models/userModel.js
+++ backend/api/models/userModel.js
@@ -10,7 +10,7 @@ const UserTagSchema = new Schema({name: String}); // need to add schema for post
 const UsersGamesSchema = new Schema({
     name: String,
     url: String,
-    gameTags: String,
+    gameTags: [String],
     gameID: String
 });
 const FriendSchema = new Schema({username: String});
